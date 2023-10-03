12de052aa534ea78a85e4c40b65d87aafdad41fe
zl90 <101400977+zl90@users.noreply.github.com>
2023-10-03 20:54:29 +1100

    Add function prototypes

--- src/dictionary_compression.h
+++ src/dictionary_compression.h
@@ -7,4 +7,6 @@
 namespace dict_compression
 {
     std::unordered_map<std::string, int> map_token_frequencies(std::vector<std::string>);
+
+    std::unordered_map<std::string, std::string> build_dictionary(std::unordered_map<std::string, int>);
 }
--- src/json.h
+++ src/json.h
@@ -2,8 +2,11 @@
 
 #include <vector>
 #include <string>
+#include <unordered_map>
 
 namespace json
 {
     std::vector<std::string> parse(std::string);
+
+    void encode(std::unordered_map<std::string, std::string>);
 }
